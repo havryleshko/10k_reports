ad60a8f2842a003f12a409c03395a07265dc6f67
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-06-16 10:21:52 +0100

    First commit

---
+++ 10k_reports.py
@@ -0,0 +1,22 @@
+import warnings
+import h2o
+from h2o import H2OFrame
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import logging
+
+h2o.init(max_mem_size='4G')
+logging.basicConfig(level=logging.INFO)
+warnings.filterwarnings('ignore')
+
+logging.info('Importing all the necessary .cvs files (10-K reports)...')
+#importing .cvs file for all companies within one sector
+
+###
+
+#STAGE 1: DATA INSPECTION
+logging.info('Starting data inspection...')
+
+
+
